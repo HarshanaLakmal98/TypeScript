114cbd268c48e897c45aa8cae2bac78d808f151d
Lakmal <harshanalakmal.work@gmail.com>
2023-02-01 10:39:09 +0530

    Number type

---
+++ app.js
@@ -0,0 +1,8 @@
+function add(num1, num2) {
+    console.log(typeof (num1));
+    return num1 + num2;
+}
+var num1 = 8;
+var num2 = 4;
+console.log(add(num1, num2));
+console.log('Lets learn ts');
---
+++ app.ts
@@ -0,0 +1,10 @@
+function add(num1 : number, num2 : number){
+   console.log(typeof(num1))
+   return num1 + num2;
+}
+
+let num1 = 8;
+let num2 = 4;
+
+console.log(add(num1,num2));
+console.log('Lets learn ts')
