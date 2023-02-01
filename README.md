a5f751d83f7c32dfe8f9aef5e36f030f0b725193
Lakmal <harshanalakmal.work@gmail.com>
2023-02-01 14:01:49 +0530

    object type

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # TypeScript Project
-### Number type
+### Object type
 ### Hi there,
 I am a beginner at Typescript and I am gonna explore the typescript.
-This branch is for all about Number Type.
+This branch is for all about Object Type.
+
+#### Reference
+[w3school](https://www.w3schools.com/typescript/typescript_object_types.php);
 
 #### Thank you!
 
--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-function add(num1, num2) {
-    console.log(typeof (num1));
-    return num1 + num2;
-}
-var num1 = 8;
-var num2 = 4;
-console.log(add(num1, num2));
-console.log('Lets learn ts');
+// TypeScript Object
+// =====================create object with data type=====================
+// const lakmal:{
+//    name:string;
+//    age:number;
+//    country:string;
+//    married:boolean;
+// } ={
+//    name:"Lakmal",
+//    age:25,
+//    country:"Sri Lanka",
+//    married:false,
+// }
+// console.log(lakmal);
+// =================create object with out define data type=================
+// const person ={
+//    fname:"Harshana",
+//    lname:"Lakmal",
+//    age:25,
+//    married:false,
+//    country:"Sri Lanka",
+// }
+// console.log(person)
+// ========== Change some value with different data type =========
+// person.age = "25"; // will show some error message
+// ============add new key value to object============
+// person.weight = "80Kg"; // will show an error message
+// ********* Solution for add new key value to object*************
+// define object with data type and assign value after defining object
+var John = {
+    fname: 'john',
+    lname: 'doe',
+    age: 32,
+    country: 'USA'
+};
+John.weight = '67kg';
+John.married = true;
+console.log(John);
+// *****************Add value after declaring object *****************
+var car = {};
+// add key value to car object
+car.brand = 'Totota';
+car.year = 2017;
+car.color = 'red';
+car.condition = 'brand new';
+console.log(car);
