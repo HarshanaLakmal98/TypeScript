c7f46ab91707fcd930aab0eb0046c6ef8e176261
Lakmal <harshanalakmal.work@gmail.com>
2023-02-01 15:32:35 +0530

    tuple type

--- app.ts
+++ app.ts
@@ -1,66 +1,9 @@
-// TypeScript Object
- 
-// =====================create object with data type=====================
+// define tuple
+let firstTuple:[number,boolean,string];
+// assign value to tuple
+firstTuple = [5,false,'lets code...']
+console.log(firstTuple);
 
-// const lakmal:{
-//    name:string;
-//    age:number;
-//    country:string;
-//    married:boolean;
-// } ={
-//    name:"Lakmal",
-//    age:25,
-//    country:"Sri Lanka",
-//    married:false,
-// }
-// console.log(lakmal);
-
-
-// =================create object with out define data type=================
-
-// const person ={
-//    fname:"Harshana",
-//    lname:"Lakmal",
-//    age:25,
-//    married:false,
-//    country:"Sri Lanka",
-// }
-// console.log(person)
-
-// ========== Change some value with different data type =========
-// person.age = "25"; // will show some error message
-
-// ============add new key value to object============
-// person.weight = "80Kg"; // will show an error message
-
-// ********* Solution for add new key value to object*************
-// define object with data type and assign value after defining object
-
-const John:{
-   fname:string,
-   lname:string,
-   age:number,
-   country:string,
-   weight?:string,
-   married?:boolean
-} = {
-   fname:'john',
-   lname:'doe',
-   age:32,
-   country:'USA',
-};
-
-John.weight='67kg';
-John.married=true;
-console.log(John)
-
-// *****************Add value after declaring object *****************
-const car:{[index:string]:(string|number|boolean)}={};
-
-// add key value to car object
-car.brand='Totota';
-car.year=2017;
-car.color='red';
-car.condition='brand new';
-
-console.log(car);
+// try to add new value to tuple
+firstTuple.push('My name is ...')
+firstTuple.push('Lets learn tuple ...')
