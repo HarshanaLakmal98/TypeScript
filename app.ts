60e25e0ed95f594f8edc503c998986e20d945b23
Lakmal <harshanalakmal.work@gmail.com>
2023-02-03 09:18:29 +0530

    modified ts file

--- app.ts
+++ app.ts
@@ -32,3 +32,36 @@ harshana = {
    age: '25'
 }
 console.log(harshana);
+
+// create any variable
+let userId: any;
+
+// assign string value
+userId='0001';
+console.log(`Assign string value : ${userId}`);
+
+// assign number value
+userId=1;
+console.log(`Assign Number value : ${userId}`);
+
+// assign number array 
+userId=[1,2,3]
+console.log(`Assign Number array : ${userId}`);
+
+// assign string array
+userId = ['1','2','3'];
+console.log(`Assign string array : ${userId}`);
+
+// assign mixed array
+userId= ['1',2,'3']
+console.log(`Assign Mixed array : ${userId}`);
+
+// assign object
+userId ={
+   name:'Lakmal',
+   id:1,
+   leader:false
+}
+console.log(`Assign object : ${userId}`);
+console.log(`Assign object : ${userId.name}`);
+console.log(userId)
